dfac0c1566edaea33eebffa44e07ec220f6cc257
black <black@example.com>
2022-11-07 15:13:58 +0500

    hello everyone

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello");
-static void GreetWhite()
-{
-    Console.WriteLine("hello white");
-}
+Console.Writи eLine("hello everyone");
