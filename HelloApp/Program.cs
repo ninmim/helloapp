66af50e6cfa6addaa327f56f17f1cf3156142d19
black <black@example.com>
2022-11-07 15:28:13 +0500

    fixes

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("hello");
+GreetWhite();
+GreetBlack();
 static void GreetWhite()
 {
     Console.WriteLine("hello white");
